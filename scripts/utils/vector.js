beed1c0ce7131547b68f3216ccf9123c4e5fcd68
Lucas Liza <lucaslizza@gmail.com>
2015-01-23 19:01:17 -0200

    created add/subtract functions

--- scripts/utils/vector.js
+++ scripts/utils/vector.js
@@ -2,4 +2,14 @@
 var Vector = function (x, y) {
 	this.x = x;
 	this.y = y;
+
+	this.add = function (vector) {
+		this.x += vector.x;
+		this.y += vector.y;
+	};
+
+	this.sub = function (vector) {
+		this.x -= vector.x;
+		this.y -= vector.y;
+	};
 };
