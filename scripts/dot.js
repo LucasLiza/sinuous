7b4250ca9bc51679c9184a7fc16621b86a9db41b
Lucas Liza <lucaslizza@gmail.com>
2015-01-23 18:40:29 -0200

    visual update

--- scripts/dot.js
+++ scripts/dot.js
@@ -1,13 +1,13 @@
 "use strict";
 var Dot = function (radius, color, position) {
-		this.radius = radius;
-		this.color = color;
-		this.position = position;
-		
-		this.draw = function (context) {
-			context.beginPath();
-      context.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI, false);
-      context.fillStyle = this.color;
-      context.fill();
-		};
+	this.radius = radius;
+	this.color = color;
+	this.position = position;
+
+	this.draw = function (context) {
+		context.beginPath();
+		context.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI, false);
+		context.fillStyle = this.color;
+		context.fill();
 	};
+};
